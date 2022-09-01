cd1128772285a07bef7f39977a2c254c137bd59c
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-09-02 01:54:30 +0300

    Succesfully used the LAT and Lon info from the first API to the second API so as to get both reading simultaneously

--- script.js
+++ script.js
@@ -2,20 +2,9 @@ const weather ={
   fetchUVIndex :function(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=65b6e2a8c3622af3622a3030c6a9e1df`)
     .then((response)=> response.json())
+    //.then((response)=> console.log(response))
     .then((data)=> this.changeUVIndex(data));
-    const options = {
-      method: 'GET',
-      headers: {
-        'x-access-token': 'fcb409146c7459a9e8f703e7e1d42d04',
-        'X-RapidAPI-Key': '1e66dc68c1msh37a5f4bbde75838p111bf0jsn9ecc11ba6306',
-        'X-RapidAPI-Host': 'aershov-openuv-global-real-time-uv-index-v1.p.rapidapi.com'
-      }
-    };
-    
-    fetch('https://aershov-openuv-global-real-time-uv-index-v1.p.rapidapi.com/api/v1/uv?lat=15&lng=15', options)
-      .then(response => response.json())
-      .then(UVData => console.log(UVData))
-      .catch(err => console.error(err));
+
   },
   
   changeUVIndex : function(data){
@@ -29,13 +18,24 @@ const weather ={
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°c"
     document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + name + "')"
-  },
-  changeUVIndex2 : function(UVData){
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-access-token': 'fcb409146c7459a9e8f703e7e1d42d04',
+        'X-RapidAPI-Key': '1e66dc68c1msh37a5f4bbde75838p111bf0jsn9ecc11ba6306',
+        'X-RapidAPI-Host': 'aershov-openuv-global-real-time-uv-index-v1.p.rapidapi.com'
+      }
+    };
     
-  }
+    fetch(`https://aershov-openuv-global-real-time-uv-index-v1.p.rapidapi.com/api/v1/uv?lat=${lat}&lng=${lon}}`, options)
+      .then(response => response.json())
+      .then(UVData => console.log(UVData))
+      .catch(err => console.error(err));
+  },
 
   search : function(){
     this.fetchUVIndex(document.querySelector(".search-bar").value);
+    //console.log(lon , lat)
   }
 };
 document.querySelector(".search button").addEventListener("click", function(){
