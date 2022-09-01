3e188c85824986eccbe661c74dd2c37c2b4f9470
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-09-01 22:20:51 +0300

    Added the second API TO MY JS FILE

--- script.js
+++ script.js
@@ -2,22 +2,38 @@ const weather ={
   fetchUVIndex :function(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=65b6e2a8c3622af3622a3030c6a9e1df`)
     .then((response)=> response.json())
-    .then((data)=> this.changeUVIndex(data))
+    .then((data)=> this.changeUVIndex(data));
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-access-token': 'fcb409146c7459a9e8f703e7e1d42d04',
+        'X-RapidAPI-Key': '1e66dc68c1msh37a5f4bbde75838p111bf0jsn9ecc11ba6306',
+        'X-RapidAPI-Host': 'aershov-openuv-global-real-time-uv-index-v1.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://aershov-openuv-global-real-time-uv-index-v1.p.rapidapi.com/api/v1/uv?lat=15&lng=15', options)
+      .then(response => response.json())
+      .then(UVData => console.log(UVData))
+      .catch(err => console.error(err));
   },
   
   changeUVIndex : function(data){
     const {name} = data;
     const {icon , description } = data.weather[0];
     const {temp } = data.main;
-    //const {lon , lat} = data.coordinates;
-    console.log (name,icon,description)
-    console.log (data.coordinates)
+    const {lon , lat } = data.coord;
+    console.log (name,icon,description,lon , lat)
     document.querySelector(".city").innerText =`UV Index in ${name}`;
     document.querySelector(".icon").src = "https://openweather.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°c"
     document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + name + "')"
   },
+  changeUVIndex2 : function(UVData){
+    
+  }
+
   search : function(){
     this.fetchUVIndex(document.querySelector(".search-bar").value);
   }
@@ -29,5 +45,6 @@ document.querySelector(".search-bar").addEventListener("key-up" , function(e){
   if(e.key == "Enter"){
     weather.search();
   }
+  
 });
 
