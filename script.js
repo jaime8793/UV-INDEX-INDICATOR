4ae8b36855caee9d1663a59fe642f307e2d316f7
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-09-02 02:38:40 +0300

    Updated the styling of the Digital Clock to be  more futuristic

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function realTime(){
+  let dateTime = new Date();
+  let hrs = dateTime.getHours();
+  let min = dateTime.getMinutes();
+  let sec = dateTime.getSeconds();
+  let session = document.querySelector('#session')
+
+  if(hrs >= 12){
+    session.innerHTML = "PM";
+  } else{
+    session.innerHTML = "AM";
+  }
+  if (hrs > 12){
+  hrs = hrs - 12
+  }
+
+  document.querySelector('#hours').innerHTML = hrs;
+  document.querySelector('#minutes').innerHTML = min;
+  document.querySelector('#seconds').innerHTML = sec;
+
+
+}
+
+setInterval(realTime, 10);
+
+
 const weather ={
   fetchUVIndex :function(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=65b6e2a8c3622af3622a3030c6a9e1df`)
